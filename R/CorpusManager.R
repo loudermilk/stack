03f2ce0d9a1aea4febdf2f198519a49d73a877e6
Brandon Loudermilk <b.c.loudermilk@gmail.com>
2016-06-03 05:45:02 -0500

    reformatting

--- R/CorpusManager.R
+++ R/CorpusManager.R
@@ -1,7 +1,7 @@
 ##################################################################
 ## CorpusManager.R
 ##
-## Easy mechanaism for create corpus given search directory and
+## Easy mechanaism to create corpus given search directory and
 ## regex expression.
 ##
 ## Example:
@@ -13,35 +13,35 @@
 ## my_corpus <- createCorpusByFiles(cm, ff)
 ## print(my_corpus)
 ##
-
-
+## Notes: Probably can deprecate this module?
+##
 
 #' @title Create FileLoader object (S3)
-#' 
+#'
 #' @description FileLoader stores a list of full pathnames
 #' to the all the files stored in dir and its subdirectories that
 #' match the pattern argument. File loader also stores a list of
 #' tags, currently defaulting to the set of unique directies under dir.
-#' 
+#'
 #' @example
 #' fl <- FileLoader("OANC-GrAF", pattern = "*.txt")
 #' fl$files
 #' fl$tags
-#' 
+#'
 #' @param dir character name of starting directory
 #' @param pattern character regex
-#' 
+#'
 #' @return FileLoader
 FileLoader <- function(dir, pattern) {
   file_list <- list.files(dir, pattern = pattern, recursive = TRUE, full.names=TRUE)
-  
-  
+
+
   #files <- sapply(file_list, FUN = basename)
   directories <- sapply(file_list, FUN = dirname)
-  
+
   tags <- unique(unlist(sapply(directories, FUN = strsplit, "/")))
-  
-  
+
+
   file_loader <- list()
   class(file_loader) <- "FileLoader"
   file_loader$files <- file_list
@@ -56,7 +56,7 @@ CorpusManager <- function(dir = ".", pattern = "*.txt") {
   cm <- list()
   class(cm) <- "CorpusManager"
   cm$file_loader <- FileLoader(dir, pattern)
-  
+
   return(cm)
 }
 
@@ -89,10 +89,10 @@ createCorpusByFiles <- function(cm, files, ...) {
 }
 
 #' @title Create VCorpus with files
-#' 
+#'
 #' @param cm CorpusManager
 #' @param character vector of files
-#' 
+#'
 #' @return VCorpus
 createCorpusByFiles.CorpusManager <- function(cm, files,...) {
   corpus_list <- lapply(files, FUN = function(f){
@@ -114,12 +114,12 @@ createCorpusByFiles.CorpusManager <- function(cm, files,...) {
 ## Primarily used as a drawing board duing development.
 ## Just uncomment and run.
 
-# 
+#
 # dir <- "OANC-GrAF"
 # pattern <- "*.txt"
 # max_records <- 10
-# 
-# 
+#
+#
 # cm <- CorpusManager(dir, pattern)
 # ff <- listFiles(cm)
 # target_files <- {
@@ -129,9 +129,9 @@ createCorpusByFiles.CorpusManager <- function(cm, files,...) {
 #     ff[1:max_records]
 #   }
 # }
-# 
-# 
-# 
+#
+#
+#
 # preprocessText <- function (cp) {
 #   cp <- tm_map(cp, stripWhitespace)
 #   cp <- tm_map(cp, content_transformer(tolower))
@@ -141,22 +141,22 @@ createCorpusByFiles.CorpusManager <- function(cm, files,...) {
 #   cp <- tm_map(cp, stripWhitespace)
 #   return(cp)
 # }
-# 
-# 
-# 
+#
+#
+#
 # cp <- createCorpusByFiles(cm, target_files)
 # cp <- preprocessText(cp)
-# 
-# 
+#
+#
 # writeLines(as.character(cp[[1]]))
-# 
+#
 # tdm <- TermDocumentMatrix(cp)
 # inspect(tdm[100:110,1:5])
-# 
+#
 # findFreqTerms(tdm, 50)
-# 
+#
 # findAssocs(tdm, "can", 0.95)
-# 
-# 
-# 
-# 
+#
+#
+#
+#
